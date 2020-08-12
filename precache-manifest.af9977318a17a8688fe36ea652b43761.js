f83f06d8469512e60c80bb4a9f5bab60aacbe678
ALisetskii <55103628+ALisetskii@users.noreply.github.com>
2020-08-12 05:04:49 +0400

    Updates

---
+++ precache-manifest.af9977318a17a8688fe36ea652b43761.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b5617c73a0e510c3a93cdede89f67f",
+    "url": "/SocialNetworkStudy/index.html"
+  },
+  {
+    "revision": "1fbc290afd6bd332f289",
+    "url": "/SocialNetworkStudy/static/css/3.20d6501d.chunk.css"
+  },
+  {
+    "revision": "629ca59126d55f70b8f2",
+    "url": "/SocialNetworkStudy/static/css/4.0fdb3ba5.chunk.css"
+  },
+  {
+    "revision": "f0cfed490dacedd131e6",
+    "url": "/SocialNetworkStudy/static/css/main.126cbbca.chunk.css"
+  },
+  {
+    "revision": "d525429bfe31cbb71ec9",
+    "url": "/SocialNetworkStudy/static/js/2.c8a4b6ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SocialNetworkStudy/static/js/2.c8a4b6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fbc290afd6bd332f289",
+    "url": "/SocialNetworkStudy/static/js/3.0258eecd.chunk.js"
+  },
+  {
+    "revision": "629ca59126d55f70b8f2",
+    "url": "/SocialNetworkStudy/static/js/4.3a80f4e7.chunk.js"
+  },
+  {
+    "revision": "f0cfed490dacedd131e6",
+    "url": "/SocialNetworkStudy/static/js/main.71989d9d.chunk.js"
+  },
+  {
+    "revision": "2a2359edd0a7e7cbf231",
+    "url": "/SocialNetworkStudy/static/js/runtime-main.e45ce8a9.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/SocialNetworkStudy/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/SocialNetworkStudy/static/media/user.8b1dbb1f.png"
+  }
+]);
